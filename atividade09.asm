60f5d0cc80d60f40de03a9648c8e7abd6eb34ec6
Almir Neto <almir.as.neto03@gmail.com>
2026-03-08 17:53:24 -0300

    terminada atividade09

---
+++ atividade09.asm
@@ -0,0 +1,17 @@
+    processor 6502
+    seg Code
+    org $F000
+
+Start:
+    lda #1
+    cld
+Loop:
+    clc
+    adc #1
+    cmp #10
+    bne Loop
+
+    jmp Start
+    org $FFFC
+    .word Start
+    .word Start
